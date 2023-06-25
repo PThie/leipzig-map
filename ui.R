009d9ea57fceb4920c244ac6a6290035d556b8eb
Patrick Thiel <patrickthiel@outlook.de>
2023-06-25 16:54:24 +0200

    Remove styling sheet becuase not needed

--- ui.R
+++ ui.R
@@ -14,7 +14,7 @@ library(sf)
 # start front end
 ui <- shinyUI(
     fluidPage(
-        tags$head(HTML("<title>Orte der Sozialdemokratie</title>"), tags$link(rel = "stylesheet", type = "text/css", href = "styling.css")),
+        tags$head(HTML("<title>Orte der Sozialdemokratie</title>")),
         leafletOutput("map", height = "100vh")
     )
 )
